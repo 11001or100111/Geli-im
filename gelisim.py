3d0a4e9abbea00d71d755b221a80cb31ae920241
11001or10011 <155253172+11001or100111@users.noreply.github.com>
2024-08-31 18:06:35 +0300

    Update gelisim.py

--- gelisim.py
+++ gelisim.py
@@ -1,1 +1,31 @@
 import subprocess
+import sys
+import socket
+import signal 
+import colorama
+yesil = colorama.Fore.GREEN
+kirmizi = colorama.Fore.RED
+siyah = colorama.Fore.BLACK
+reset = colorama.Fore.RESET
+
+print(f"{yesil} Hoşgeldin Reis :) {reset}")
+
+oyun = """ {}
+1) 
+2)
+3) 
+4)
+5)
+6)
+7)
+{}"""
+
+gerekliler = """ {}
+1)
+2
+3
+4
+5
+6
+7)
+{} """
